df8894a60ac98b804edaeca0ba99a26845e372b5
domram558 <71326962+domram558@users.noreply.github.com>
2020-09-21 23:23:55 +0300

    Added checks for data entry; Replaced the algorithm for calculating statistics.

--- Analist.cs
+++ Analist.cs
@@ -1,128 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
-using System.Text;
 
-namespace SmartSaver
+namespace Plutus
 {
     class Analist
     {
-        string[] categories = {
-            "Food",
-            "Clothes",
-            "Bills",
-            "Other"};
-
-        int[] collected =
-        {
-            0, 0, 0, 0};
-        public Analist()
-        {
-           // string catNames = Form1.giveCategories();
-        }
-
         public string analise(string database)
         {
-            string data = "none";
-            for(int i = 0; i < 4; i++)
+            string data = "";
+            double total = 0;
+            Dictionary<string, double> sums = new Dictionary<string, double>();
+
+            foreach (string category in Enum.GetNames(typeof(Categories)))
             {
-                collected[i] = 0;
+                sums.Add(category, 0);
             }
-            int sum = 0;
-            parseData(database);
-            foreach(int i in collected)
+
+            string[] db = database.Split('$');
+
+            for (int i = 0; i < db.Length - 4; i += 4)
             {
-                sum = sum + i;
+                string date = db[i].Replace(">", "");
+                string name = db[i + 1];
+                double price = Convert.ToDouble(db[i + 2]);
+                string category = db[i + 3];
+
+                sums[category] += price;
+                total += price;
             }
-            double a = (double)collected[0] / (double)sum;
-            double b = (double)collected[1] / (double)sum;
-            double c = (double)collected[2] / (double)sum;
-            double d = (double)collected[3] / (double)sum;
-            a = a * 100;
-            b = b * 100;
-            c = c * 100;
-            d = d * 100;
 
-            data = "Food - " + collected[0] + " (" + a.ToString("f1") + "%)" + "@Clothes - " + collected[1] + " (" + b.ToString("f1") + "%)" + "@Bills - " + collected[2] + " (" + c.ToString("f1") + "%)" + "@Other - " + collected[3] + " (" + d.ToString("f1") + "%)";
-            data = data.Replace("@", System.Environment.NewLine);
-
-
-            return data;
-        }
-        private void parseData(string database)
-        {
-            int len = database.Length;
-            int dolCount = 0; ;
-            string priceWord = "";
-            int price = 0;
-            string category = "";
-            int nextDol = 0;
-            for (int i = 0; i < len; i++)
+            foreach (string category in Enum.GetNames(typeof(Categories)))
             {
-                if(database[i] == '>')
-                {
-                    dolCount = 0;
-                    category = "";
-                    priceWord = "";
-                    price = 0;
-                }
-                if (database[i] == '$')
-                {
-                    i++;
-                    dolCount++;
-                } 
-                if (dolCount == 2 && priceWord == "")
-                {
-                    nextDol = 0;
-                    for(int y = 0; nextDol == 0; y++)
-                    {
-                        if(database[i + y] == '$')
-                        {
-                            priceWord = database.Substring(i, y);
-                            System.Diagnostics.Debug.Write("price - ");
-                            System.Diagnostics.Debug.WriteLine(priceWord);
-                            nextDol = 1;
-                        }
-                    }
-                }
-                if (dolCount == 3 && category == "")
-                {
-                    nextDol = 0;
-                    for (int y = 0; nextDol == 0; y++)
-                    {
-                        if (database[i + y] == '$')
-                        {
-                            category = database.Substring(i, y);
-                            System.Diagnostics.Debug.Write("category - ");
-                            System.Diagnostics.Debug.WriteLine(category);
-                            nextDol = 1;
-                        }
-                    }
-                }
-                if (dolCount == 4 && price == 0)
-                {
-                    price = Int32.Parse(priceWord);
-                    switch (category)
-                    {
-                        case "Food":
-                            collected[0] = collected[0] + price;
-                            break;
-                        case "Clothes":
-                            collected[1] = collected[1] + price;
-                            break;
-                        case "Bills":
-                            collected[2] = collected[2] + price;
-                            break;
-                        case "Other":
-                            collected[3] = collected[3] + price;
-                            break;
-                        default:
-                            System.Diagnostics.Debug.Write("unknown category");
-                            break;
-                    }
-                }
-
+                string percent = String.Format("{0:0.00}", sums[category] / total * 100);
+                data += category + " " + sums[category] + " (" + percent + "%)" + System.Environment.NewLine;
             }
+
+            return data != "" ? data : "No data found!";
         }
     }
 }
---
+++ GUI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Plutus
+{
+    enum Categories
+    {
+        Food,
+        Clothes,
+        Bills,
+        Other
+    }
+    public partial class GUI : Form
+    {
+        DataManager manager = new DataManager();
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String answer = ">" + DateTime.Now + "$" + nameP.Text + "$" + priceP.Text + "$" + categoryP.Text + "$";
+            if (nameP.Text.Length == 0 || nameP.Text == null)
+            {
+                statScreen.Text = "Name cannot be empty!";
+                return;
+            }
+            if (!System.Text.RegularExpressions.Regex.IsMatch(priceP.Text, @"\d."))
+            {
+                statScreen.Text = "Price must be a number!";
+                return;
+            }
+            if (categoryP.Text.Length == 0 || categoryP == null)
+            {
+                statScreen.Text = "Please choose a category!";
+                return;
+            }
+            statScreen.Text = nameP.Text + " was successfully added!";
+            nameP.Text = null;
+            priceP.Text = null;
+            categoryP.Text = null;
+            manager.write(answer);
+        }
+
+        private void showData_Click(object sender, EventArgs e)
+        {
+            String data = manager.giveData();
+            string[] db = data.Split('$');
+
+            data = "";
+            for (int i = 0; i < db.Length - 4; i += 4)
+            {
+                string date = db[i].Replace(">", "");
+                string name = db[i + 1];
+                double price = Convert.ToDouble(db[i + 2]);
+                string category = db[i + 3];
+
+                data += date + " | " + name + " | " + price + "€ | " + category;
+            }
+            statScreen.Text = data;
+        }
+
+        private void showStat_Click(object sender, EventArgs e)
+        {
+            String data = manager.giveAnalisis();
+            statScreen.Text = data;
+        }
+
+    }
+}
