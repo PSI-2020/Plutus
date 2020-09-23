8fa7748c8be8435c7e194e0156f3e3332c5271d3
domram558 <71326962+domram558@users.noreply.github.com>
2020-09-23 11:52:12 +0300

    Changed analist to match new coding standart

--- Analist.cs
+++ Analist.cs
@@ -7,34 +7,33 @@ namespace Plutus
     {
         public string analise(string database)
         {
-            string data = "";
-            double total = 0;
-            Dictionary<string, double> sums = new Dictionary<string, double>();
+            var data = "";
+            var total = 0.0;
+            var sums = new Dictionary<string, double>();
 
             foreach (string category in Enum.GetNames(typeof(Categories)))
             {
                 sums.Add(category, 0);
             }
 
-            string[] db = database.Split('$');
+            var db = database.Split('$');
 
-            for (int i = 0; i < db.Length - 4; i += 4)
+            for (var i = 0; i < db.Length - 4; i += 4)
             {
-                string date = db[i].Replace(">", "");
-                string name = db[i + 1];
-                double price = Convert.ToDouble(db[i + 2]);
-                string category = db[i + 3];
+                //var date = db[i].Replace(">", "");
+                //var name = db[i + 1];
+                var price = Convert.ToDouble(db[i + 2]);
+                var category = db[i + 3];
 
                 sums[category] += price;
                 total += price;
             }
 
-            foreach (string category in Enum.GetNames(typeof(Categories)))
+            foreach (var category in Enum.GetNames(typeof(Categories)))
             {
-                string percent;
-                if (total == 0) percent = " (" + String.Format("{0:0.00}", 0) + "%)";
-                else percent = " (" + String.Format("{0:0.00}", sums[category] / total * 100) + "%)";
-
+                var percent = total == 0
+                    ? " (" + String.Format("{0:0.00}", 0) + "%)"
+                    : " (" + String.Format("{0:0.00}", sums[category] / total * 100) + "%)";
                 data += category + " " + sums[category] + percent + System.Environment.NewLine;
             }
 
