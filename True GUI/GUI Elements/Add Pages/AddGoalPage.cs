b7c62501ff07fe4f92fea84ee429541fb394b5c4
Simona <simona.salatkaite@gmail.com>
2020-11-02 23:03:47 +0200

    adding new goal GUI

---
+++ True GUI/GUI Elements/Add Pages/AddGoalPage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Plutus
+{
+    public partial class TrueGUI : Form
+    {
+        private Label goalNameFieldLabel;
+        private Label goalAmountFieldLabel;
+        private Label goalDueDateFieldLabel;
+        private Button addGoalButton;
+        private TextBox goalNameBox;
+        private TextBox goalAmountBox;
+        private DateTimePicker goalDueDateBox;
+
+        private void LoadAddGoalPage()
+        {
+            LoadEscapeButtonField();
+
+            goalNameFieldLabel = CreateClassicLabel("goalNameFieldLabel", "Name:", Color.White, lilitaOne, 18F, 272, 40, 50, 100, 1, ContentAlignment.BottomLeft);
+            goalAmountFieldLabel = CreateClassicLabel("goalAmountFieldLabel", "Amount you want to save:", Color.White, lilitaOne, 18F, 500, 40, 50, 260, 3, ContentAlignment.BottomLeft);
+            goalDueDateFieldLabel = CreateClassicLabel("goalDueDateFieldLabel", "Set a due date:", Color.White, lilitaOne, 18F, 272, 40, 50, 420, 5, ContentAlignment.BottomLeft);
+            addGoalButton = CreateClassicButton("addPaymentButton", "ADD", Color.White, lilitaOne, 14F, firstColor, 272, 80, 50, 650, 7);
+            addGoalButton.Click += new EventHandler(addGoalButton_Click);
+            goalNameBox = CreateTextField("goalNameBox", "Enter Name", 160, 2);
+            goalAmountBox = CreateTextField("goalAmountBox", "0", 320, 4);
+            goalDueDateBox = new DateTimePicker();
+            goalDueDateBox.SetBounds(50, 480, 272, 80);
+            goalDueDateBox.Font = new Font(lilitaOne, 18F, FontStyle.Regular, GraphicsUnit.Point);
+            errorMessage = CreateClassicLabel("errorLabel", "", firstColor, lilitaOne, 13F, 272, 40, 50, 530, 1, ContentAlignment.MiddleCenter);
+
+            Controls.Add(goalNameFieldLabel);
+            Controls.Add(goalAmountFieldLabel);
+            Controls.Add(addGoalButton);
+            Controls.Add(goalNameBox);
+            Controls.Add(goalAmountBox);
+            Controls.Add(goalDueDateFieldLabel);
+            Controls.Add(goalDueDateBox);
+        }
+
+        private void addGoalButton_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(errorMessage);
+            var goalManager = new GoalManager();
+            var verify = new InputVerification();
+            var error = verify.VerifyData(name: goalNameBox.Text, amount: goalAmountBox.Text);
+            if (error != "")
+            {
+                errorMessage.Text = error;
+                Controls.Add(errorMessage);
+            }
+            else
+            {
+                goalManager.AddGoal(goalNameBox.Text, goalAmountBox.Text, goalDueDateBox.Value);
+                Controls.Clear();
+                LoadGoalsPage();
+            }
+        }
+
+       
+    }
+}
--- True GUI/GUI Elements/Menu Pages/GoalsPage.cs
+++ True GUI/GUI Elements/Menu Pages/GoalsPage.cs
@@ -1,12 +1,15 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Plutus
 {
     public partial class TrueGUI : Form
     {
-        Label goalsPageName;
-
+        private Label goalsPageName;
+        private Label myGoalsTextLabel;
+        private Button newGoalAddButton;
+        private FlowLayoutPanel myGoalsPanel;
 
         private void LoadGoalsPage()
         {
@@ -14,10 +17,58 @@ namespace Plutus
             LoadEscapeButtonCat();
 
             goalsPageName = CreatePageNameLabel("goalsPageNameLabel", "GOALS");
+            myGoalsTextLabel = CreateClassicLabel("myGoalsTextLabel", "My Goals:", Color.White, lilitaOne, 24F, ClientSize.Width, 60, 0, 120, 1);
+            newGoalAddButton = CreateClassicButton("newGoalAddButton", Properties.Resources.PlusButton, 150, ClientSize.Height - 100, 2);
+            newGoalAddButton.Click += new EventHandler(AddNewGoalButton_Click);
 
+            myGoalsPanel = new FlowLayoutPanel
+            {
+                //BackColor = secondColor,
+                Width = ClientSize.Width - 70,
+                Left = 35,
+                Top = 200,
+                Height = ClientSize.Height - 320,
+                FlowDirection = FlowDirection.LeftToRight
+            };
+
+            Controls.Add(myGoalsPanel);
+            Controls.Add(newGoalAddButton);
             Controls.Add(goalsPageName);
+            Controls.Add(myGoalsTextLabel);
+            LoadMyCurrentGoals();
             ResumeLayout(false);
             PerformLayout();
+        }
+
+        private void AddNewGoalButton_Click(object sender, EventArgs e)
+        {
+            Controls.Clear();
+            LoadAddGoalPage();
+        }
+
+        private void LoadMyCurrentGoals()
+        {
+            var goalManager = new GoalManager();
+            var list = goalManager.ReadGoals();
+
+            foreach(var goal in list)
+            {
+                /* var button = CreateClassicButton("goal", goal.Name, Color.White, lilitaOne, 14F, firstColor, 145, 80, 50, 650, 3);
+                 myGoalsPanel.Controls.Add(button);
+                 button.Click += new EventHandler(CheckMyGoal);*/
+
+                var button = new GoalButton(goal.Name, Color.White, lilitaOne, 14F, firstColor, 145, 80, goal);
+                myGoalsPanel.Controls.Add(button);
+                button.Click += new EventHandler(CheckMyGoal);
+            }
+        }
+
+        private void CheckMyGoal(object sender, EventArgs e)
+        {
+            GoalButton button = (sender as GoalButton);
+            Controls.Clear();
+            LoadCheckGoalPage(button.Goal);
+
         }
     }
 }
