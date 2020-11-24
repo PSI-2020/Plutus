393626ffb31ecd3e92dc013e6be2399827ba733b
Dominykas <dom.ram558@gmail.com>
2020-11-24 13:42:32 +0200

    History service updated

--- True GUI/GUI Elements/Menu Pages/HistoryPage.cs
+++ True GUI/GUI Elements/Menu Pages/HistoryPage.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Plutus
 {
@@ -13,11 +17,12 @@ namespace Plutus
         ComboBox historyPaymentTypeBox;
         DataGridView historyDataGrid;
 
-
         private void InitializeHistoryPage()
         {
             historyPageName = CreatePageNameLabel("historyPageNameLabel", "HISTORY");
             historyEditButton = CreateClassicButton("historyEditButton", Properties.Resources.EditButton, 45, 160, 3, false, 30, 30);
+            historyEditButton.Click += new EventHandler(EditButtonClick);
+
             historyPaymentTypeBox = new ComboBox
             {
                 Name = "historyPaymentTypeBox",
@@ -53,18 +58,22 @@ namespace Plutus
                 BorderStyle = BorderStyle.None,
                 ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single,
 
+                SelectionMode = DataGridViewSelectionMode.FullRowSelect,
+                MultiSelect = false,
                 EnableHeadersVisualStyles = false,
                 RowHeadersVisible = false,
                 ReadOnly = true,
-                MultiSelect = false,
                 AllowUserToResizeColumns = false,
                 AllowUserToResizeRows = false
             };
 
             historyDataGrid.DefaultCellStyle.BackColor = _backgroundColor;
             historyDataGrid.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            historyDataGrid.DefaultCellStyle.SelectionBackColor = _secondColor;
 
-            historyDataGrid.ColumnHeadersDefaultCellStyle.Font = new Font(_lilitaOne, 17, FontStyle.Regular, GraphicsUnit.Pixel); ;
+            historyDataGrid.ColumnHeadersDefaultCellStyle.Font = new Font(_lilitaOne, 17, FontStyle.Regular, GraphicsUnit.Pixel);
+            historyDataGrid.ColumnHeadersDefaultCellStyle.SelectionBackColor = _firstColor;
+            historyDataGrid.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.White;
             historyDataGrid.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
             historyDataGrid.ColumnHeadersDefaultCellStyle.BackColor = _firstColor;
             historyDataGrid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
@@ -110,6 +119,48 @@ namespace Plutus
             }
 
             historyDataGrid.DataSource = dataSource;
+        }
+
+        private void EditButtonClick(object sender, EventArgs e)
+        {
+            //Controls.Clear();
+            //LoadMenuButton();
+            //LoadEscapeButton();
+            //Controls.Add(CreatePageNameLabel("historyEditPageName", "Edit"));
+            //ResumeLayout(false);
+            //PerformLayout();
+
+            var rowIndex = historyDataGrid.CurrentCell.RowIndex;
+            var payment = new Payment
+            {
+                Date = DateTime.Parse(historyDataGrid.Rows[rowIndex].Cells[0].Value.ToString()).ConvertToInt(),
+                Name = historyDataGrid.Rows[rowIndex].Cells[1].Value.ToString(),
+                Amount = double.Parse(historyDataGrid.Rows[rowIndex].Cells[2].Value.ToString()),
+                Category = historyDataGrid.Rows[rowIndex].Cells[3].Value.ToString()
+            };
+
+            var newPayment = new Payment
+            {
+                Date = DateTime.UtcNow.ConvertToInt(),
+                Name = historyDataGrid.Rows[rowIndex].Cells[1].Value.ToString(),
+                Amount = double.Parse(historyDataGrid.Rows[rowIndex].Cells[2].Value.ToString()),
+                Category = historyDataGrid.Rows[rowIndex].Cells[3].Value.ToString()
+            };
+
+            switch (historyPaymentTypeBox.SelectedIndex)
+            {
+                case 0:
+                    _fileManager.EditPayment(payment, newPayment, historyDataGrid.Rows[rowIndex].Cells[4].Value.ToString());
+                    return;
+                case 1:
+                    _fileManager.EditPayment(payment, newPayment, "Expense");
+                    return;
+                case 2:
+                    _fileManager.EditPayment(payment, newPayment, "Income");
+                    return;
+                default:
+                    return;
+            }
         }
     }
 }
