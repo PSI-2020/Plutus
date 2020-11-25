7b6da61bd97529464577eb797eafbb0901971057
RokasBagdonas <r.bagdonas2000@gmail.com>
2020-11-25 15:33:26 +0200

    test

---
+++ WebAPI/Models/PaymentsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class PaymentsContext : DbContext
+    {
+        public PaymentsContext(DbContextOptions<PaymentsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebPayment> WebPayments { get; set; }
+    }
+}
